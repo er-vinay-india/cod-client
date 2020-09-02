5b8e283e5cf1dddf63142d368c90566366233b25
vinaysharmaaeione <vinaysharma@aeione.com>
2020-09-02 10:39:06 +0530

    removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LeftAreaComponent } from './left-area/left-area.component';
-import { MainAreaComponent } from './main-area/main-area.component';
 import { ProductComponent } from './product/product.component';
 import { ProductLeftComponent } from './product-left/product-left.component';
 import { ProductRightComponent } from './product-right/product-right.component';
 import { HomeComponent } from './home/home.component';
 import { CarouselBoxComponent } from './carousel-box/carousel-box.component';
 import { AccordianBoxComponent } from './accordian-box/accordian-box.component';
-import { LeftBoxComponent } from './left-box/left-box.component';
-import { RightBoxComponent } from './right-box/right-box.component';
 import { UpcomingBoxComponent } from './upcoming-box/upcoming-box.component';
 import { NewsletterBoxComponent } from './newsletter-box/newsletter-box.component';
 import { ProductBoxComponent } from './product-box/product-box.component';
@@ -31,16 +27,12 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 @NgModule({
   declarations: [
     AppComponent,
-    LeftAreaComponent,
-    MainAreaComponent,
     ProductComponent,
     ProductLeftComponent,
     ProductRightComponent,
     HomeComponent,
     CarouselBoxComponent,
     AccordianBoxComponent,
-    LeftBoxComponent,
-    RightBoxComponent,
     UpcomingBoxComponent,
     NewsletterBoxComponent,
     ProductBoxComponent,
